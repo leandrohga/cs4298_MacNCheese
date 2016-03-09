8a7cb0cf9788d1e127c5b054166f38f83b1b1477
Tsumikiri <afurey0@gmail.com>
2016-03-09 08:25:21 -0600

    Scanner Tester
    Started the Scanner Tester program.

---
+++ scantester.cpp
@@ -0,0 +1,28 @@
+/**
+ *
+ * @date 9-mar-2016
+ */
+
+ #include <iostream>
+ #include <fstream>
+ #include <string>
+ #include <ctime>
+ using namespace std;
+
+#include "mscan.h"
+
+void main() {
+    ifstream source;
+    ofstream output;
+    Scanner scan;
+
+    /*
+    Pseudo-code:
+        Open the file
+        While not end of file
+            Get next token from scanner
+            Print out token and buffer
+     */
+     
+    return 0;
+}
