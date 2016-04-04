e9b1635ee7c2202fd265b96397be9554c26137e3
Tsumikiri <afurey0@gmail.com>
2016-04-04 08:46:23 -0500

    Added case for PLUS_OP & MINUS_OP
    We can now shout integer arithmetic literal expressions, but the order
    of operations is just left-to-right instead of PEMDAS.

--- mparse.cpp
+++ mparse.cpp
@@ -271,13 +271,23 @@ void Parser::FactorTail(ExprRec& result)
 		code.GenInfix(leftOperand, op, rightOperand, result);
 		FactorTail(result);
 		break;
+	case PLUS_OP:
+	case MINUS_OP:
+		leftOperand.kind = result.kind;
+		leftOperand.ival = result.ival; /* FIXME TODO check other types */
+		leftOperand.name = result.name;
+		AddOp();
+		code.ProcessOp(op);
+		Primary(rightOperand);
+		/*** CODE ***/
+		code.GenInfix(leftOperand, op, rightOperand, result);
+		FactorTail(result);
+		break;
 	case RSTAPLE:
 	case RBANANA:
 	case RMUSTACHE:
 	case SEMICOLON:
 	case COMMA:
-	case PLUS_OP:
-	case MINUS_OP:
 	case LT_OP:
 	case LE_OP:
 	case GT_OP:
