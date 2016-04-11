17936168cf4c40c76b35b8f3a7b22b4d0a95ed8f
Tsumikiri <afurey0@gmail.com>
2016-04-11 08:19:34 -0500

    Updated run script

--- mncRun.sh
+++ mncRun.sh
@@ -2,20 +2,15 @@
 if [ "$1" = "help" ]
 then
     echo "This script automatically builds all of the required executables, compiles the mnc files, assembles the asm files, and runs the obj files."
-    echo "Usage: ./mncRun.sh <inputFiles>"
-    echo "       ./mncRun.sh clobber"
-    echo "       ./mncRun.sh help"
-    echo "Example: ./mncRun.sh simple shoutTest declarations"
+    echo "Usage: ./mncRun.sh <inputFiles>              Compile and run the input files."
+    echo "       ./mncRun.sh <inputFiles> clobber      Compile and run the input files, then clean up."
+    echo "       ./mncRun.sh clobber                   Clean up generated files."
+    echo "       ./mncRun.sh help                      Show this help message."
+    echo "Example: ./mncRun.sh shoutAddition shoutFloatAssign clobber"
     echo "The clobber option cleans up (removes) the compiled executables."
     echo "Notes: "
     echo "    Do not include the .mnc extension."
     echo "    The input files are assumed to be in the tests folder."
-elif [ "$1" = "clobber" ]
-then
-    echo ">>> cd tests"
-    cd tests
-    echo ">>> rm -f *.lis *.lst *.asm *.obj macc sam micro"
-    rm -f *.lis *.lst *.asm *.obj macc sam micro
 else
     echo ">>> make build"
     make build
@@ -23,12 +18,18 @@ else
     cd tests
     for var in "$@"
     do
-        echo ">>> micro $var.mnc"
-        ./micro $var.mnc
-        echo ">>> sam $var.asm"
-        ./sam $var.asm
-        echo ">>> macc $var.obj"
-        ./macc $var.obj
+        if [ "$var" = "clobber" ]
+        then
+            echo ">>> rm -f *.lis *.lst *.asm *.obj macc sam micro"
+            rm -f *.lis *.lst *.asm *.obj macc sam micro
+        else
+            echo ">>> micro $var.mnc"
+            ./micro $var.mnc
+            echo ">>> sam $var.asm"
+            ./sam $var.asm
+            echo ">>> macc $var.obj"
+            ./macc $var.obj
+        fi
     done
 fi
 echo ">>> done"
