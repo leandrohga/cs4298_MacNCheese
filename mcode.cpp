e51ece906ba553a01eca5e76fe97056a26d02946
Nelson <manklar@hotmail.com>
2016-04-17 23:12:28 -0500

    string 1000 N 0

--- mcode.cpp
+++ mcode.cpp
@@ -67,8 +67,7 @@ void CodeGen::Enter(ExprRec& var) {
 		break;
 	case CHEESE:
 		variable.size = 1024; /* 4x8 = 32 bits */
-		variable.sval = var.sval;
-		cerr << variable.sval << " " << variable.size ;
+		variable.sval = "\""+var.sval+"\"";
 		//Added this into the mcode.h before now here :)
 		/* TODO please check: check what to do. Check for cheese? */
 		break;
